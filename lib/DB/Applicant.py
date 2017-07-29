b5a9715c80b8d748010e74407280423563dc5f52
Dominik Herbst <herbst@s-beat.de>
2017-07-29 18:52:34 +0200

    Fix copyright header

--- lib/DB/Applicant.py
+++ lib/DB/Applicant.py
@@ -1,4 +1,21 @@
-# S-BEAT Project by Annkristin Stratmann, Niclas Steigelmann, Dominik Herbst
+"""
+Copyright (c) 2017 S-BEAT GbR and others and others
+
+This file is part of S-BEAT.
+
+S-BEAT is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+S-BEAT is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with S-BEAT. If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import array
 import csv
